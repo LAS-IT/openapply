b3744c4f5b47dce7ac5e12b709ccc3d51d93243a
Bill Tihen <btihen@gmail.com>
2018-04-10 10:40:28 +0200

    quick fix for local usage

--- spec/openapply_version_spec.rb
+++ spec/openapply_version_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Openapply::Version do
       expect(Openapply::Version::VERSION).not_to be nil
     end
     it "has correct version number" do
-      expect(Openapply::Version::VERSION).to eq "0.3.2"
+      expect(Openapply::Version::VERSION).to eq "0.3.1"
     end
 
   end
